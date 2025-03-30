8c5cc92db8a31d1297e12f9f367ea63809252bee
Reza Vali <80775511+rzvl@users.noreply.github.com>
2025-03-31 01:16:50 +0330

    fix: remove import of role-gate.tsx into index.ts file

--- src/app/account/admin/page.tsx
+++ src/app/account/admin/page.tsx
@@ -1,4 +1,4 @@
-import { RoleGate } from '@/components/features/auth'
+import { RoleGate } from '@/components/features/auth/role-gate'
 
 export default function AdminPage() {
   return <RoleGate allowedRoles={['admin']}>Admin Page</RoleGate>
--- src/components/features/auth/index.ts
+++ src/components/features/auth/index.ts
@@ -4,7 +4,6 @@ import { ForgotPasswordForm } from './forgot-password-form'
 import { OAuthButton } from './oauth-button'
 import { ResetPasswordForm } from './reset-password-form'
 import { ResendEmailVerificationButton } from './resend-email-vrf-btn'
-import { RoleGate } from './role-gate'
 import { UsernameSetupForm } from './username-setup-form'
 import { VerifyEmailMessage } from './verify-email-message'
 
@@ -15,7 +14,6 @@ export {
   OAuthButton,
   ResetPasswordForm,
   ResendEmailVerificationButton,
-  RoleGate,
   UsernameSetupForm,
   VerifyEmailMessage,
 }
